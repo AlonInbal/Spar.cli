8542162114ae842e98a9c5f468576fb81e398f62
Callum Bartlett <coolum444@hotmail.co.uk>
2017-05-29 23:36:34 +0100

    :sparkles: Added ability to auto-delete invites
    This might be useful. It isn't logged to anywhere, but it is something
    that's noticed now.

--- Cogs/AutoModerator.py
+++ Cogs/AutoModerator.py
@@ -7,14 +7,41 @@ class AutoModerator:
     def __init__(self, sparcli):
         self.sparcli = sparcli
 
+    async def deleteServerInvtes(self, message):
+        '''
+        Automatically delete server invites
+        '''
+
+        # Check the server configs
+        serverSettings = getServerJson(message.server.id)
+        deleteInvites = serverSettings['Toggles']['Deleteserverinvites']
+        if not deleteInvites:
+            return
+
+        # See if the message contains a server invite
+        splitMessage = message.content.lower().split('discord.gg/')
+        if len(splitMessage) == 1:
+            return
+
+        # There's one or more server invite in there
+        # Check for an existing invite code
+        if len(splitMessage[1].split(' ')[0]) == 0:
+            return
+
+        # There's an invite code
+        await self.sparcli.delete_message(message)
+        
+
+    async def on_message(self, message):
+        await self.deleteServerInvtes(message)
+
     async def on_member_update(self, before, after):
         '''
         Check for any forced nicknames
         '''
 
         serverSettings = getServerJson(after.server.id)
-        automodSettings = serverSettings['AutoModerator']
-        forcedNicknames = automodSettings['ForcedNicknames']
+        forcedNicknames = serverSettings['ForcedNicknames']
 
         # `forcedNicknames` is a {userID: nickname} pair
         try:
