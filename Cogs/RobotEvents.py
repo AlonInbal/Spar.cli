cc6e017c43f959ca34a9999d0d79076ab14723d9
Callum Bartlett <coolum444@hotmail.co.uk>
2017-07-18 16:39:12 +0100

    Added vexnextevents command
    It lets you get the next event in a given location.

--- Cogs/RobotEvents.py
+++ Cogs/RobotEvents.py
@@ -26,6 +26,11 @@ class RobotEvents(object):
         team = quote(teamNumber, safe='')
         async with self.session.get(base.format(team)) as r:
             rawData = await r.json()
+
+        if rawData['status'] == 0:
+            await self.sparcli.say('That is not a valid team number.')
+            return
+
         data = rawData['result'][0]
 
         o = OrderedDict()
@@ -38,8 +43,42 @@ class RobotEvents(object):
         o['Grade'] = data['grade']
         o['Registered'] = str(bool(data['is_registered']))
         em = makeEmbed(fields=o, author=data['team_name'])
-        # em = makeEmbed(author='test')
-        # await self.sparcli.say(o)
+        await self.sparcli.say(embed=em)
+
+    @commands.command(pass_context=True)
+    async def vexnextevents(self, ctx, *, location:str):
+        '''
+        Gives you the next five VEX events in a given country
+        '''
+
+        await self.sparcli.send_typing(ctx.message.channel)
+
+        base = 'https://api.vexdb.io/v1/get_events?limit_number=5&limit_start=0&country={}'
+        place = quote(location, safe='')
+        async with self.session.get(base.format(place)) as r:
+            rawData = await r.json()
+
+        if rawData['status'] == 0:
+            await self.sparcli.say('That is not a valid location.')
+            return
+
+        data = reversed(rawData['result'])
+
+        o = OrderedDict()
+        for i in data:
+            i['start'] = i['start'].split('T')[0]
+            o[i['key']] = '**Name:** {name}\n' \
+                '**Date:** {start}\n' \
+                '**Postcode:** {postcode}\n' \
+                '**RobotEvents Page:** [Bwap](' \
+                'https://www.robotevents.com/robot-competitions/' \
+                'vex-robotics-competition/{id}.html)'.format(
+                    name=i['name'],
+                    start=i['start'],
+                    postcode=i['loc_postcode'],
+                    id=i['key']
+                )
+        em = makeEmbed(fields=o, inline=False)
         await self.sparcli.say(embed=em)
 
 
