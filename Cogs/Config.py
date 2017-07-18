e60a74a41a0692461a4f12ea311026f2f9ae7ad2
Callum Bartlett <coolum444@hotmail.co.uk>
2017-07-18 16:35:36 +0100

    Added a makestarboard command
    This automatically makes a starboard channel for you so you don't need
    to set one manually.

--- Cogs/Config.py
+++ Cogs/Config.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from discord import Channel
+from discord import Channel, PermissionOverwrite
 from Cogs.Utils.Messages import getTextRoles
 from Cogs.Utils.Configs import getServerJson, saveServerJson
 from Cogs.Utils.GuiConfig import addEmojiList, updateFromEmoji, updateFromMessage
@@ -81,7 +81,7 @@ class Config:
         await self.sparcli.say('Done!')
 
     @commands.command(pass_context=True)
-    @permissionChecker(check='manage_roles')
+    @permissionChecker(check='manage_roles', aliases=['joinbotrole'])
     async def botjoinrole(self, ctx, *, roleName:str):
         '''
         Sets a role to be given to a user when they join
@@ -92,6 +92,22 @@ class Config:
 
         serverSettings = getServerJson(ctx.message.server.id)
         serverSettings['OnBotJoin'] = roleObject.id
+        saveServerJson(ctx.message.server.id, serverSettings)
+        await self.sparcli.say('Done!')
+
+    @commands.command(pass_context=True)
+    @permissionChecker(check='manage_channels')
+    async def makestarboard(self, ctx):
+        '''
+        Make a channel that works with the starboard setup
+        '''
+
+        serverSettings = getServerJson(ctx.message.server.id)
+        serverSettings['Toggles']['Starboard'] = True
+        c = PermissionOverwrite(send_messages=False, add_reactions=False)
+        d = PermissionOverwrite(send_messages=True, add_reactions=True)
+        v = await self.sparcli.create_channel(ctx.message.server, 'starboard', (ctx.message.server.default_role, c), (ctx.message.server.me, d))
+        serverSettings['Channels']['Starboard'] = v.id
         saveServerJson(ctx.message.server.id, serverSettings)
         await self.sparcli.say('Done!')
 
