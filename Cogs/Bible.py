13c59cc10ac255d0a8ac1a91d8193e817f4426da
Callum Bartlett <coolum444@hotmail.co.uk>
2017-07-22 23:13:31 +0100

    Redone bible commands
    Now using corect amounts of regex and commenting, the bot is now fully
    able to get things from the Bible nad Quran very nicely.

--- Cogs/Bible.py
+++ Cogs/Bible.py
@@ -1,136 +1,149 @@
-from aiohttp import ClientSession 
-from json import loads 
-from collections import OrderedDict
 from re import finditer
-from discord.ext import commands 
+from json import loads
+from collections import OrderedDict
+from aiohttp import ClientSession
+from urllib.parse import quote, unquote
+from discord.ext import commands
 from Cogs.Utils.Messages import makeEmbed
 
 
-class Scriptures:
+def match(pattern, string):
+    q = finditer(pattern, string)
+    try:
+        return list(q)[0]
+    except IndexError:
+        return None
 
-    def __init__(self, sparcli):
-        self.sparcli = sparcli
-        self.bible = 'https://getbible.net/json?scrip={}'
+
+class Scriptures(object):
+
+    def __init__(self, sparcli:commands.Bot):
+        self.sparcli = sparcli 
+        self.session = ClientSession(loop=sparcli.loop)
+        self.regexMatches = {
+            'OriginalRequest': r'(.+[a-zA-Z0-9]+\s[0-9]+:[0-9]+(-[0-9]+)?)',
+            'StripAuthor': r'(.+\b)([0-9]+:)',
+            'GetPassages': r'([0-9]+:[0-9]+)',
+            'GetMax': r'(-[0-9]+)',
+            'QuranMatch': r'([0-9]+:[0-9]+([-0-9]+)?)'
+        }
         self.biblePicture = 'http://pacificbible.com/wp/wp-content/uploads/2015/03/holy-bible.png'
         self.quranPicture = 'http://www.siotw.org/modules/burnaquran/images/quran.gif'
-        self.session = ClientSession(loop=sparcli.loop)
 
     def __unload(self):
-        self.session.close()  
-
-    async def getBiblePassage(self, passage):
-        '''
-        Goes through the getbible api to get a list of applicable Bible passages
-        '''
-
-        # Format the URL string
-        toRetrieveFrom = self.bible.format(passage.replace(' ', '%20'))
-        
-        # Send the request to the site and return it as a JSON dictionary
-        async with self.session.get(toRetrieveFrom) as r:
-            text = await r.text()
-        return loads(text[1:-2])
-
-    async def getQuranPassage(self, passage):
-        '''Goes through the alquran api to get quran passages'''
-        bible = 'http://api.alquran.cloud/ayah/{}/en.sahih'
-        toRetrieveFrom = bible.format(passage)
-        # data = get(toRetrieveFrom).content
-        async with self.session.get(toRetrieveFrom) as r:
-            data = await r.json()
-        return data
-
-
-    @commands.command(pass_context=True, aliases=['christianity', 'bible'])
-    async def christian(self, ctx, *, passage:str):
-        '''
-        Gets a passage from the Bible
-        '''
-
-        # TODO: MAKE ALL THIS CLEANER TO WORK WITH
-        await self.sparcli.send_typing(ctx.message.channel)
-
-        # Generate the string that'll be sent to the site
-        getString = passage
-
-        # Work out how many different quotes you need to get
-        matches = finditer(r'[\d]+', passage)
-        matchList = [i for i in matches]
-        if len(matchList) == 2:
-            passage = int(matchList[1].group())
-            lastpassage = passage 
-        elif len(matchList) == 3:
-            passage = int(matchList[1].group())
-            lastpassage = int(matchList[2].group())
-        else:
-            await self.sparcli.say('I was unable to get that passage.')
-            return
-
-        # Actually go get all the data from the site
-        try:
-            bibleData = await self.getBiblePassage(getString)
-        except Exception:
-            await self.sparcli.say('I was unable to get that passage.')
-            return
-
-        # Get the nice passages and stuff
-        passageReadings = OrderedDict()
-        chapterName = bibleData['book'][0]['book_name']
-        chapterPassages = bibleData['book'][0]['chapter']
-        chapterNumber = bibleData['book'][0]['chapter_nr']
-        for i in range(passage, lastpassage + 1):
-            passageReadings['{}:{}'.format(chapterNumber, i)] = chapterPassages[str(i)]['verse']
-
-        # Make it into an embed
-        em = makeEmbed(fields=passageReadings, author_icon=self.biblePicture, author=chapterName)
-
-        # Boop it to the user
-        await self.sparcli.say('', embed=em)
+        self.session.close()
 
     @commands.command(pass_context=True)
-    async def quran(self, ctx, *, passage: str):
+    async def bible(self, ctx, *, script:str):
         '''
-        Gets a passage from the Quran
+        Gives you the Christian Bible quote from a specific script
         '''
 
+        # Check if it's a valid request
+        matches = match(self.regexMatches['OriginalRequest'], script)
+        if not matches:
+            self.sparcli.say('That string was malformed, and could not be processed. Please try again.')
+            return
+        else:
+            script = matches.group()
+
+        # It is - send it to the API
         await self.sparcli.send_typing(ctx.message.channel)
 
-        # Generate the string that'll be sent to the site
-        getString = passage
+        # Actually do some processing
+        script = quote(script, safe='')
+        async with self.session.get('https://getbible.net/json?scrip={}'.format(script)) as r:
+            try:
+                apiText = await r.text()
+                apiData = loads(apiText[1:-2])
+            except Exception as e:
+                apiData = None
+        script = unquote(script)
 
-        # Work out how many different quotes you need to get
-        tempPass = passage.split(':')[1]  # Gets the 34-35 from 14:34-35
-        if len(tempPass.split('-')) == 2:
-            passage = int(tempPass.split('-')[0])
-            lastpassage = int(tempPass.split('-')[1])
+        # Just check if it's something that we can process
+        if not apiData:
+            await self.sparcli.say('I was unable to get that paricular Bible passage. Please try again.')
+
+        # Now we do some processin'
+        # Get the max and the min verse numbers
+        chapterMin = int(match(self.regexMatches['GetPassages'], script).group().split(':')[1])
+        chapterMax = match(self.regexMatches['GetMax'], script)
+        if chapterMax:
+            chapterMax = int(chapterMax.group()) + 1
         else:
-            passage = lastpassage = int(tempPass)
+            chapterMax = chapterMin + 1
 
-        # Actually go get all the data from the site
-        bibleData = await self.getQuranPassage(getString)
-
-        # Get the nice passages and stuff
-        passageReadings = OrderedDict()
-        chapterName = bibleData['data']['surah']['englishName']
-        chapterPassages = bibleData['data']
-        chapterNumber = bibleData['data']['surah']['number']
-        for i in range(passage, lastpassage + 1):
-            getThisTurn = '{}:{}'.format(chapterNumber, i)
-            bibleData = await self.getQuranPassage(getThisTurn)
-            y = bibleData['data']['text']
-            passageReadings[getThisTurn] = y
+        # Process them into an ordered dict
+        o = OrderedDict()
+        passages = apiData['book'][0]['chapter']
+        for verse in range(chapterMin, chapterMax):
+            o[verse] = passages[str(verse)]['verse']
 
         # Make it into an embed
-        em = makeEmbed(
-            fields=passageReadings,
-           author_icon=self.quranPicture, 
-           author=chapterName, 
-           colour=0x258D58
-        )
+        author = match(self.regexMatches['StripAuthor'], script).group().title()
+        em = makeEmbed(fields=o, author=author, author_icon=self.biblePicture)
 
-        # Boop it to the user
-        await self.sparcli.say('', embed=em)
+        # And done c:
+        await self.sparcli.say(embed=em)
+
+    async def getQuran(self, number, mini, maxi, english=True):
+        '''
+        Sends stuff off to the API to be processed, returns embed object
+        '''
+
+        o = OrderedDict()
+        if english:
+            base = 'http://api.alquran.cloud/ayah/{}:{}/en.sahih'
+        else:
+            base = 'http://api.alquran.cloud/ayah/{}:{}'
+
+        # Get the data from the API
+        async with self.session.get(base.format(number, mini)) as r:
+            data = await r.json()
+
+        author = data['data']['surah']['englishName'] if english else data['data']['surah']['name']
+        o['{}:{}'.format(number, mini)] = data['data']['text']
+
+        for verse in range(mini+1, maxi):
+            async with self.session.get(base.format(number, verse)) as r:
+                data = await r.json()
+            o['{}:{}'.format(number, verse)] = data['data']['text']
+
+        em = makeEmbed(fields=o, author=author, author_icon=self.quranPicture)
+        return em
+
+    @commands.command(pass_context=True)
+    async def quran(self, ctx, *, script:str):
+        '''
+        Gives you a Quran quote given a specific verse
+        '''
+
+        # http://api.alquran.cloud/ayah/{}/en.sahih
+
+        # Check if it's a valid request
+        matches = match(self.regexMatches['QuranMatch'], script)
+        if not matches:
+            self.sparcli.say('That string was malformed, and could not be processed. Please try again.')
+            return
+        else:
+            script = matches.group()
+
+        # It is - prepare to send it to an API
+        await self.sparcli.send_typing(ctx.message.channel)
+
+        # Acutally do some processing
+        number = int(script.split(':')[0])
+        minQuote = int(script.split(':')[1].split('-')[0])
+        try:
+            maxQuote = int(script.split(':')[1].split('-')[1]) + 1
+        except IndexError as e:
+            maxQuote = minQuote + 1
+
+        # Send it off nicely to the API to be processed
+        em = await self.getQuran(number, minQuote, maxQuote)
+        await self.sparcli.say(embed=em)
 
 
-def setup(bot):
-    bot.add_cog(Scriptures(bot))
+def setup(sparcli):
+    x = Scriptures(sparcli)
+    sparcli.add_cog(x)
