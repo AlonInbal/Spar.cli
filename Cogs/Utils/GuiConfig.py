00cb402e304037f0d09c7a298b8fc2d10abab6e3
Callum Bartlett <coolum444@hotmail.co.uk>
2017-07-23 04:49:36 +0100

    Update the guiconfig util for the Dpy rewrite God, that's an ugly util. I knew I wanted to rewrite it but damn. That's... damn. That's terrible. Both it and the config cog should be rewritten.

--- Cogs/Utils/GuiConfig.py
+++ Cogs/Utils/GuiConfig.py
@@ -1,36 +1,38 @@
-async def addEmojiList(sparcli, message, emojiList):
-    '''Adds a list of emoji to a given message'''
+THUMBS = ['👍', '👎']
 
-    # Split emoji into a list if it is a string
-    if type(emojiList) == str:
-        emojiList = list(emojiList)
 
-    # Add the reactions
+async def addEmojiList(message, emojiList):
+    '''
+    Adds a list of emoji to a message
+    '''
+
     for i in emojiList:
-        await sparcli.add_reaction(message, i)
+        await message.add_reaction(message, i)
 
 
-async def updateFromEmoji(sparcli, ctx, serverSettings, thingToEnable, whatPresentlyIs):
-    '''Configures a serverconfig depending on a given emoji'''
+async def updateFromEmoji(sparcli, ctx, serverSettings, enableQuery, currentStatus):
+    '''
+    Configures a serverconfig depending on a given emoji
+    '''
 
     # Shorten a line
-    author = ctx.message.author
-    thumbs = ['👍', '👎']
+    author = ctx.author
 
     # Print out message to user
-    mes = await sparcli.say('Enable {0}? (Presently `{1}`)'.format(thingToEnable, whatPresentlyIs))
+    mes = await ctx.send('Enable {0}? (Presently `{1}`)'.format(enableQuery, currentStatus))
 
     # Add emoji to it
-    await addEmojiList(sparcli, mes, thumbs)
+    await addEmojiList(mes, THUMBS)
 
     # See what the user gives back
-    ret, usr = await sparcli.wait_for_reaction(thumbs, user=author, message=mes)
+    check = lambda r, u: r.emoji in THUMBS and u == author and r.message == mes
+    r, _ = await sparcli.wait_for('reaction_add', check=check)
 
     # Set it up to save
-    serverSettings['Toggles'][thingToEnable] = {'👍': True, '👎': False}[ret.emoji]
+    serverSettings['Toggles'][thingToEnable] = {'👍': True, '👎': False}[r.emoji]
 
     # Delete the message
-    await sparcli.delete_message(mes)
+    await mes.delete()
 
     return serverSettings
 
@@ -39,27 +41,42 @@ async def updateFromMessage(sparcli, ctx, serverSettings, thingToSet):
     '''Configures a serverconfig depending on a given message'''
 
     # Shorten a line
-    author = ctx.message.author
-    channel = ctx.message.channel
+    author = ctx.author
+    channel = ctx.channel
+    limit = 0
 
     # Say out to user
     mes = [await sparcli.say('What channel should {0} be set to?'.format(thingToSet))]
 
     # Wait for response from user
     while True:
-        ret = await sparcli.wait_for_message(author=author, channel=channel)
+        ret = await sparcli.wait_for('message', author=author, channel=channel)
 
         # Check if there's a tagged channel
         mentioned = ret.channel_mentions
-        if mentioned == []:
+        if not mentioned:
+
+            # There's not - up a counter
+            limit += 1
+            if limit == 5:
+                await ctx.send('You\'ve failed this prompt too many times - aborting.')
+                return None
+
+            # Tell them they need to not be a lil shit
             z = await sparcli.say('You need to tag a channel in your message.')
             mes.append(z)
             mes.append(ret)
+
         else:
-            for q in mes:
-                await sparcli.delete_message(q)
-            await sparcli.delete_message(ret)
+
+            # There's a tagged channel - delete the messages
+            for q in mes + [ret]:
+                await q.delete()
+
+            # Save the settings
             serverSettings['Channels'][thingToSet] = mentioned[0].id
+
+            # Exit the loop
             break
 
     return serverSettings
